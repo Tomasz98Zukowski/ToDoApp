32fe71cdb4f59a595e091af2169225536cd55c65
TomaszZukowski <tomaszzukowskibp@gmail.com>
2023-12-19 23:01:44 +0100

    Task 22 - udemy done

--- First/Program.cs
+++ First/Program.cs
@@ -4,8 +4,15 @@
     {
         static void Main(string[] args)
         {
-            int? a = null;
-            Console.WriteLine(a.GetValueOrDefault());
+            List<int> list = new List<int>() { 1,2,3,4,5};
+            List<string> list2 = new List<string>() { "dzien", "dobry","Kladia" };
+            foreach (int i in list) { Console.WriteLine(i); }
+            foreach(string i in list2) { Console.WriteLine(i); }
+            Console.WriteLine(list.Max());
+            list.Sort();
+            list2.Sort();
+
+
 
         }
     }
