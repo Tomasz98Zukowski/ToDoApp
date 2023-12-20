3f25992c704eb4304f2316f7b7683aa5eeeadf21
TomaszZukowski <tomaszzukowskibp@gmail.com>
2023-12-20 19:39:50 +0100

    Try catch lesson - start

---
+++ TryCatch_01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TryCatch_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ThrowException(0);
+            }
+            catch (InvalidOperationException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+        static void ThrowException(int id)
+        {
+            if (id == 0) { throw new ArgumentNullException("id"); }
+        }
+
+        
+    }
+}
