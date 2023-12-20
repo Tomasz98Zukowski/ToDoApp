c5494fbc8ca2734c9f8af7eca61c45c78b0c636e
TomaszZukowski <tomaszzukowskibp@gmail.com>
2023-12-20 01:05:47 +0100

    First time with stringbulider

--- lesson23/Program.cs
+++ lesson23/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace lesson23
+﻿using System.Text;
+
+namespace lesson23
 {
     internal class Program
     {
@@ -6,6 +8,28 @@
         {
             Priority priority = Priority.low;
             Console.WriteLine(priority);
+            string appendCharacter = "text";
+
+            int[] samples = new int[2] { 100000, 1000000 };
+            foreach (int sample in samples)
+            {
+                var currentTime = DateTime.UtcNow;
+                testAppendString(sample, new StringBuilder(), appendCharacter);
+                var endTime = DateTime.UtcNow;
+                
+                Console.WriteLine($"Time:{endTime.Subtract(currentTime)}");
+            }
+
+            static void testAppendString(int n, StringBuilder str, string appendCharacter)
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    str.Append(appendCharacter);
+                }
+            }
+
+            
+
 
         }
     }
